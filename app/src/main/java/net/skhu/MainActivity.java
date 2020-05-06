79433708a63406bd9f61adedb7abcad80a3cb49e
Yoon, DeuckGi <isz03072@gmail.com>
2020-05-06 14:07:07 +0900

    목록화면 예제 구현(시험용)

---
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -0,0 +1,28 @@
+package net.skhu;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnListView_clicked(View view) {
+        Intent intent = new Intent(this, ListViewActivity.class);
+        startActivity(intent);
+    }
+    public void btnRecyclerView1_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerView1Activity.class);
+        startActivity(intent);
+    }
+    public void btnRecyclerView2_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerView2Activity.class);
+        startActivity(intent);
+    }
+    public void btnRecyclerView3_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerView3Activity.class);
+        startActivity(intent);
+    }
+}
